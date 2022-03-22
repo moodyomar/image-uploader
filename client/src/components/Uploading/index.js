e935eac32db127f58b2676300d547241ac8ff397
Mohammad Omar Shihab <lonak.secret@gmail.com>
2022-03-22 19:35:22 +0200

    add multer in server, upload comps

---
+++ client/src/components/Uploading/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Uploading.css'
+
+
+const Uploading = () => { 
+
+return(
+
+<div className='Uploading'>
+<h1>Uploading...</h1>
+<div className="loader">
+    <div className="filler"></div>
+</div>
+</div>
+
+)
+}
+
+export default Uploading
